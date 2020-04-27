77ffb538c38307c2706b795f7ad9cc115f6b1b04
huigjr <huigjr@gmail.com>
2020-04-27 18:55:46 +0200

    Update

--- models/PageModel.php
+++ models/PageModel.php
@@ -3,6 +3,10 @@ class PageModel extends BaseModel{
 
   protected $table = 'pages';
   
+  public $pageid;
+  public $title;
+  public $content;
+  
   public function init(){
     if(isset($_POST['page'])) $this->updatePage($this->slug);
     $result = $this->slug && !is_numeric($this->slug) ? $this->hydrateModel('url') : $this->hydrateModel('pageid');
@@ -10,6 +14,14 @@ class PageModel extends BaseModel{
     if($result === false) $this->return404();
   }
   
+  public function newPage(){
+    $this->db->dbWrite(
+      "UPDATE `pages` SET `title` = :title, `content` = :content WHERE `pages`.`pageid` = :id", 
+      array('title' => $_POST['title'],'content' => $_POST['content'],'id' => $id)
+    );
+    header("Location: /dashboard/cms");
+  }
+
   private function updatePage($id){
     $this->db->dbWrite("UPDATE `pages` SET `title` = :title, `content` = :content WHERE `pages`.`pageid` = :id", array('title' => $_POST['title'],'content' => $_POST['content'],'id' => $id));
     header("Location: /dashboard/cms");
